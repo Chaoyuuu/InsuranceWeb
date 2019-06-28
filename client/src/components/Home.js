febc4c8f2795f31cb625b93769db1f50b564cf1d
Chaoyuuu <a0935292660@gmail.com>
2019-06-28 14:48:53 +0800

    redesign UI home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-// import { Button } from 'reactstrap';
-
-import {Navbar, Nav, NavDropdown, Form, FormControl, Button, Container, Carousel} from "react-bootstrap";
+import "./css/Home.css";
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button, Container, Carousel} from "react-bootstrap";
+import { Card, Row, Col, Media, Jumbotron, Image} from "react-bootstrap";
 
 class Home extends Component {
     constructor(props){
@@ -18,8 +18,6 @@ class Home extends Component {
         return (
             
             <div>               
-               
-                {/* <Container> */}
                 <Navbar bg="light" expand="lg">
                 <Navbar.Brand href="https://github.com/Chaoyuuu/InsuranceWeb">Insurance Dapp</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -44,48 +42,165 @@ class Home extends Component {
                     </Form>
                 </Navbar.Collapse>
                 </Navbar>
-                {/* </Container>  */}
-                <h3>Home page :-)</h3>
 
+                {/* Intro */}
+                <Carousel id="Intro">
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://i.imgur.com/WBtrdgk.jpg"
+                        alt="First slide"
+                        />
+                        <Carousel.Caption className="carousel-content">
+                            <Row><h3>First slide label</h3></Row>
+                            <Row><p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p></Row>
+                        </Carousel.Caption>
+                    </Carousel.Item>
 
-                <Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://i.imgur.com/eAOeogN.jpg"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://i.imgur.com/bDxQpOD.jpg"
-      alt="Third slide"
-    />
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://i.imgur.com/WBtrdgk.jpg"
+                        alt="Third slide"
+                        />
+                        <Carousel.Caption className="carousel-content">
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://i.imgur.com/WBtrdgk.jpg"
+                        alt="Third slide"
+                        />
+                        <Carousel.Caption className="carousel-content">
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
 
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://i.imgur.com/Y0ytK8r.jpg"
-      alt="Third slide"
-    />
+          
+                {/* About */}
+                <Jumbotron fluid id="about">
+                    <Container className="about-container">
+                        <Row> 
+                            <Col className="col-md-offset-12 col-md-12 titlee">
+                                <h2> What is Insurance Dapp </h2>
+                                <div className="heading-line"></div>
+                                <p> a website with blockchain </p>
+                            </Col>
+                        </Row>
 
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
+                        <Media as="li">
+                            <img
+                            width={512}
+                            height={300}
+                            className="mr-4 img"
+                            src="https://i.imgur.com/yFd5QDg.jpg"
+                            alt="Generic placeholder"
+                            />
+                            <Media.Body className="body">
+                            <h5>List-based media object</h5>
+                            <p>
+                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+                                ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+                                tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                                fringilla. Donec lacinia congue felis in faucibus.
+                            </p>
+                            </Media.Body>
+                        </Media>
+                    </Container>
+                </Jumbotron>
 
+                <Image id="pall-img" src="https://i.imgur.com/HQQFUTX.jpg" fluid />             
+
+                {/* tool */}
+                <Container id="tool">
+                    <Row> 
+                        <Col className="col-md-offset-12 col-md-12 tilte">
+                            <h2> Tools </h2>
+                            <div className="heading-line"></div>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col xs={12} sm={3} md={3} lg={3} className="card_1">
+                            <a href="https://github.com/Chaoyuuu/InsuranceWeb">
+                            <Image
+                                width={250}
+                                height={250} 
+                                src="https://i.imgur.com/wzIfRRo.jpg/" roundedCircle/>
+                            </a>
+                            <h3>ethereum</h3>
+                            <p>the text </p>
+                        </Col>
+                        <Col xs={12} sm={3} md={3} lg={3} className="card_2">
+                            <a href="https://github.com/Chaoyuuu/InsuranceWeb">
+                            <Image 
+                                width={250}
+                                height={250} 
+                                src="https://i.imgur.com/wzIfRRo.jpg/" roundedCircle />
+                            </a>
+                            <h3>ethereum</h3>
+                            <p>the text </p>
+                        </Col>
+                        <Col xs={12} sm={3} md={3} lg={3} className="card_3">
+                            <a href="https://github.com/Chaoyuuu/InsuranceWeb">
+                            <Image 
+                                width={250}
+                                height={250} 
+                                src="https://i.imgur.com/wzIfRRo.jpg/" roundedCircle/>
+                            </a>
+                            <h3>ethereum</h3>
+                            <p>the text </p>
+                        </Col>
+                        <Col xs={12} sm={3} md={3} lg={3} className="card_4">
+                            <a href="https://github.com/Chaoyuuu/InsuranceWeb">
+                            <Image 
+                                width={250}
+                                height={250} 
+                                src="https://i.imgur.com/wzIfRRo.jpg/" roundedCircle/>
+                            </a>
+                            <h3>ethereum</h3>
+                            <p>the text </p>
+                        </Col>
+                    </Row>
+                </Container>
+
+                {/* about_me */}
+                <Jumbotron fluid id="about_me">
+                    <Container className="about-container">
+                        <Row> 
+                            <Col className="col-md-offset-12 col-md-12 titlee">
+                                <h2> What is Insurance Dapp </h2>
+                                <div className="heading-line"></div>
+                                <p> a website with blockchain </p>
+                            </Col>
+                        </Row>
+
+                        <Media as="li">
+                            <img
+                            width={512}
+                            height={300}
+                            className="mr-4 img"
+                            src="https://i.imgur.com/yFd5QDg.jpg"
+                            alt="Generic placeholder"
+                            />
+                            <Media.Body className="body">
+                            <h5>List-based media object</h5>
+                            <p>
+                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+                                ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+                                tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                                fringilla. Donec lacinia congue felis in faucibus.
+                            </p>
+                            </Media.Body>
+                        </Media>
+                    </Container>
+                </Jumbotron>
 
 
              
