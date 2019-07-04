be66cbfcf53c7380118eea477f56d9b511d098e7
Chaoyuuu <a0935292660@gmail.com>
2019-07-04 23:38:41 +0800

    map json to array & show table

--- client/src/components/MyContract.js
+++ client/src/components/MyContract.js
@@ -4,25 +4,6 @@ import NavBar from "./NavBar.js"
 import getWeb3 from "../utils/getWeb3";
 import { Table, Container, Button } from "react-bootstrap";
 
-const SetTable = props => (
-    <tr>
-        {/* <td className={props.c._contract ? 'completed' : ''}>{props.todo.todo_description}</td>
-        <td className={props.c._start ? 'completed' : ''}>{props.todo.todo_responsible}</td>
-        <td className={props.c._due ? 'completed' : ''}>{props.todo.todo_priority}</td>
-        <td className={props.c._action ? 'completed' : ''}>{props.todo.todo_priority}</td>
-        <td>
-            <Link to={"/edit/"+props.todo._id}>Edit</Link>
-        </td> */}
-
-        <td>{props.c._contract}</td>
-        <td>{props.c._start}</td>
-        <td>{props.c._due}</td>
-        <td>{props.c._action}</td>
-        <td>link !!! </td>
-    </tr>
-)
-
-
 class MyContract extends Component{
 
     constructor(props) {
@@ -31,84 +12,78 @@ class MyContract extends Component{
           storageValue: null,
           web3: null,
           accounts: null,
-          my_Constract:[],
-        //   contract: null
+          my_Constract:[]
         }
-        // this.setContract = this.setContract.bind(this)
     }
     
-        componentDidMount = async() => {
-            try {
-                
-                // Get network provider and web3 instance.
-                const web3 = await getWeb3();
+    componentDidMount = async() => {
+        try {
             
-                // Use web3 to get the user's accounts.
-                const accounts = await web3.eth.getAccounts();
-                console.log(`account ${accounts}`)
-                
-                this.setState({ web3, accounts: accounts });
+            // Get network provider and web3 instance.
+            const web3 = await getWeb3();
+        
+            // Use web3 to get the user's accounts.
+            const accounts = await web3.eth.getAccounts();
+            console.log(`account ${accounts}`)
+            
+            this.setState({ web3, accounts: accounts });
 
 
-                const self = this
-                axios.get('http://localhost:5000/api/items/'+this.state.accounts)
-                    .then(function(response) {
-                        console.log('Successfully connected to db in get didmount')
-                        console.log(response.data)
+            const self = this
+            axios.get('http://localhost:5000/api/items/'+this.state.accounts)
+                .then(function(response) {
+                    console.log('Successfully connected to db in get didmount')
+                    console.log(response.data)
 
-
-
-
-                        // let topbarLinks = myJson.topbarLinks.map((topbarLinks, key) => {
-                        //     return (
-                        //       <Link
-                        //         key={topbarLinks.id}
-                        //         text={topbarLinks.text}
-                        //         icon={topbarLinks.icon}
-                        //         link={topbarLinks.link}
-                        //       />
-                        //     )
-                        //   })
-                        //   this.setState({topbarLinks: topbarLinks});  // <--
-                            
-                        // var test = Object.keys(response.data[0]).map(function(k) { return (response.data[0])[k] });
-                        // console.log(typeof(test))
-                        // console.log(`test is ${test["_id"]}`)
-
-                        let tmp = response.data.map((topbarLinks, key) => {
-                            console.log(`in map ${topbarLinks._id}`)
-                            return (
-                                
-                                <tr key={topbarLinks._id}>
-                                <td> _id = {topbarLinks._id}/</td>
-                                <td> _start = {topbarLinks._start}</td>
-                                <td> _due = {topbarLinks._due}</td>
-                                <td>_link = {topbarLinks._contract}</td>
-                                </tr>
-                                
-                            )
+                    /* JSON to array - example 1
+                    let topbarLinks = myJson.topbarLinks.map((topbarLinks, key) => {
+                        return (
+                            <Link
+                            key={topbarLinks.id}
+                            text={topbarLinks.text}
+                            icon={topbarLinks.icon}
+                            link={topbarLinks.link}
+                            />
+                        )
                         })
-
-                        self.setState({my_Constract: tmp});
-                                    
-                        // console.log(typeof(test))
-                        // console.log(`test is ${test["_id"]}`)
-
-                        // response.data[0].map(obj => this.setState({ my_Constract: Object.values(obj) }))
+                        this.setState({topbarLinks: topbarLinks});  
+                    */
                         
-                    })
-                    .catch((err, res) => {
-                        console.log(`Not connected to db in put${err}`)
-                    });
+                    /* JSON to array - example 2
+                    var test = Object.keys(response.data[0]).map(function(k) { return (response.data[0])[k] });
+                    console.log(typeof(test))
+                    console.log(`test is ${test["_id"]}`)
+                    */
 
-            } catch (error) {
-                // Catch any errors for any of the above operations.
-                alert(
-                    `Failed to load web3, accounts. Check console for details. in MyContract`,
-                );
-                console.error(error);
-            }
-        };
+                    let tmp = response.data.map((topbarLinks, key) => {
+                        console.log(`in map ${topbarLinks._id}`)
+                        return (
+                            
+                            <tr key={topbarLinks._id}>
+                            <td> _id = {topbarLinks._id}/</td>
+                            <td> _start = {topbarLinks._start}</td>
+                            <td> _due = {topbarLinks._due}</td>
+                            <td>_link = {topbarLinks._contract}</td>
+                            </tr>
+                            
+                        )
+                    })
+
+                    self.setState({my_Constract: tmp});
+                    
+                })
+                .catch((err, res) => {
+                    console.log(`Not connected to db in put${err}`)
+                });
+
+        } catch (error) {
+            // Catch any errors for any of the above operations.
+            alert(
+                `Failed to load web3, accounts. Check console for details. in MyContract`,
+            );
+            console.error(error);
+        }
+    };
 
 
     render(){
@@ -122,8 +97,6 @@ class MyContract extends Component{
                 
                 {/* <h2> code : {this.state.my_Constract}</h2> */}
             
-                
-                {/* <GetContract a={this.state}/> */}
                 <Container>
                     <Table striped bordered hover>
                         <thead>
@@ -141,147 +114,6 @@ class MyContract extends Component{
                     </Table>
                 </Container>
 
-            </div>
-        );
-    }
-};
-
-
-class GetContract extends Component{
-
-    constructor(props) {
-        super(props);    
-        this.state = {
-            // my_Constract:[],
-            addr: ''
-        }
-
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.SetList = this.SetList.bind(this);
-        
-    }
-
-    
-    handleSubmit (e) {
-        e.preventDefault();
-        console.log(`is in handle, addr ${this.props.a.accounts}`)
-        var test
-        axios.get('http://localhost:5000/api/items/'+this.props.a.accounts)
-            .then(function(response) {
-                console.log('Successfully connected to db in get didmount')
-                console.log(response.data)
-                test = response.data
-                console.log(test)
-                
-                // console.log(`Successfully connected to db in get ${res.data}`)
-                
-            })
-            .catch((err, res) => {
-                console.log(`Not connected to db in put${err}`)
-            });
-
-        this.setState({my_Constract: test})
-    }
-
-    // componentDidMount = async () => {
-    //     console.log('hiiiii')
-    //     axios.get('http://localhost:5000/api/items/'+this.state.addr)
-    //         .then(function(response) {
-    //             console.log('Successfully connected to db in get didmount')
-    //             console.log(response)
-    //             this.setState({my_Constrat: response.data})
-    //             // console.log(`Successfully connected to db in get ${res.data}`)
-                
-    //         })
-    //         .catch((err, res) => {
-    //             console.log(`Not connected to db in put${err}`)
-    //         });
-    // }
-
-    // componentDidUpdate() {
-    //     axios.get('http://localhost:5000/api/items/'+this.state.addr)
-    //     .then(function(response) {
-    //         console.log('Successfully connected to db in get didupdate')
-    //         console.log(response)
-    //         this.setState({my_Constrat: response.data})
-    //     })
-    //     .catch(function (error) {
-    //         console.log(error);
-    //     })   
-    // }
-
-    // ContractList() {
-    //     return this.state.my_Constract.map(function(currentList, i) {
-    //         return <SetTable c={currentList} key={i} />;
-    //     });
-    // }
-
-    
-    // SetList(props) {
-    //     const numbers = props.numbers;
-    //     const listItems = this.state.my_Constrat.map((number) =>
-    //       <li key={number.toString()}>
-    //         {number}
-    //       </li>
-    //     );
-    //     return (
-    //       <ul>{listItems}</ul>
-    //     );
-    //   }
-
-    // componentDidUpdate() {
-    //         axios.get('http://localhost:5000/api/items/'+this.state.addr)
-    //         .then(function(response) {
-    //             console.log('Successfully connected to db in get didupdate')
-    //             console.log(response)
-    //             this.setState({my_Constrat: response.data})
-    //         })
-    //         .catch(function (error) {
-    //             console.log(error);
-    //         })   
-    // }
-
-     SetList(){
-        console.log(`in setlist ${this.props.a.my_Constract}`)
-        const ContractList = this.props.a.my_Constract.map((my_C) => 
-            <SetTable c={my_C}/>
-            // <tr>
-            //     <td>{my_C._contract}</td>
-            // <td>{my_C._start}</td>
-            // <td>{my_C._due}</td>
-            // <td>{my_C._action}</td>
-            // <td>link !!! </td>
-            // </tr>
-        );
-
-        return( 
-            <ul>{ContractList}</ul> 
-        );
-    }
-
-    render(){
-        return (
-            <div>
-                <h3> MyContract </h3>
-                <Container>
-                    <Table striped bordered hover>
-                        <thead>
-                            <tr>
-                                {/* <th>#</th> */}
-                                <th>My Contract</th>
-                                <th>Start Date</th>
-                                <th>Due Date</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        
-                            { this.SetList() }
-                        </tbody>
-                    </Table>
-
-                    <Button onClick={this.handleSubmit } > click </Button>
-                </Container>
             </div>
         );
     }
